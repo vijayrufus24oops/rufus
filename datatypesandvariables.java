41e876daa05d877b457462d608a433c158286551
vijayrufus24oops <vijayrufus24@gmail.com>
2025-10-28 19:34:10 +0530

    Add files via upload

---
+++ datatypesandvariables.java
@@ -0,0 +1,12 @@
+public class datatypesandvariables {
+
+    public static void main(String args[])
+{
+    int a=10;
+    int b=20;
+    int c=30;
+    int d=a+b+c;
+    System.out.println(d);
+    
+}
+}
