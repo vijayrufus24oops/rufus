fcc6a5000f8e33c450987b32b6ef9de3f58e15e4
vijayrufus24oops <vijayrufus24@gmail.com>
2025-10-28 19:36:31 +0530

    Update datatypesandvariables.java

--- datatypesandvariables.java
+++ datatypesandvariables.java
@@ -6,7 +6,10 @@ public class datatypesandvariables {
     int b=20;
     int c=30;
     int d=a+b+c;
+    String e = "vijay behaves good"
     System.out.println(d);
+    System.out.println(e);
     
 }
+
 }
