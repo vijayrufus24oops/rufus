cbd5caf662ff2b9a6d5b81ada2a52b09a77a972c
vijayrufus24oops <vijayrufus24@gmail.com>
2025-11-02 21:31:48 +0530

    Add files via upload

---
+++ oe.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+
+class oe{
+    public static void main(String args[])
+    {
+        int a=2,b=1,c=(a>b)?a:b;
+    if(c==a)
+    System.out.println("true");
+    else if (c==b) 
+    System.out.println("false");
+            
+        }
+    }
+
