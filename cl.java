c1e8debca1cc4fbd17aea41041af31f6cc69cb9c
vijayrufus24oops <vijayrufus24@gmail.com>
2025-11-11 19:59:37 +0530

    Add files via upload

---
+++ cl.java
@@ -0,0 +1,10 @@
+public class cl {
+    public static void main(String[] args){
+        System.out.println(args[0] );
+
+        int a= Integer.parseInt(args[1]);
+
+        System.out.println(a);
+    }
+    
+}
