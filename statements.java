769b355668187e9b36073a1d2cc3bd0c185d2284
vijayrufus24oops <vijayrufus24@gmail.com>
2025-11-11 15:44:57 +0530

    Add files via upload

---
+++ statements.java
@@ -0,0 +1,16 @@
+public class statements {
+     public static void main(String[]args){
+        int num=2;
+        switch (num) {
+            case 1:
+            System.out.println("the num is 1");
+            break;
+            case 2:
+            System.out.println("the num is 2");
+            break;
+            default:
+          System.out.println("no numbers found");
+        }
+    }
+    
+}
