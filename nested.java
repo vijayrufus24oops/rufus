63c83aa8f67d15d943ef1114298b988469e03c16
vijayrufus24oops <vijayrufus24@gmail.com>
2025-11-11 15:37:55 +0530

    Add files via upload

---
+++ nested.java
@@ -0,0 +1,21 @@
+public class nested {
+    public static void main(String[]args){
+        String city = "chennai";
+        if (city == "delhi"){
+        System.out.println("it is delhi");
+    }
+    else if(city== "mumbai"){
+        System.out.println("it is mumbai");
+    }
+    else if(city== "chennai"){
+        System.out.println("it is chennai");
+
+    }
+    else{
+        System.out.println("you are not in chennai");
+    }
+
+
+
+    }
+}
