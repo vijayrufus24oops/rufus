472db3c7e16365afbeaff93f932c7557ad699bbb
vijayrufus24oops <vijayrufus24@gmail.com>
2025-11-11 18:22:50 +0530

    Add files via upload

---
+++ parameter.java
@@ -0,0 +1,21 @@
+public class parameter {
+
+    void soap(int money)
+    {
+        System.out.println("Soap is purchased");
+    }
+    void chocolate(int money)
+    {
+        System.out.println("Chocolate is purchased");
+
+    }
+    public static void main(String[]args){
+
+        parameter obj1 = new parameter();
+        obj1.chocolate(20);
+    }
+
+
+
+    }
+    
